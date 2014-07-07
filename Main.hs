2253ccf9d199907b483f33bb30f222919e83380c
alan hawkins <hawk.alan@gmail.com>
2014-07-07 13:19:39 +1000

    memo example

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+
 import qualified Evolution_01_Freshman
 import qualified Evolution_02_Sophomore
 import qualified Evolution_03_Junior
@@ -25,17 +26,30 @@ import qualified Evolution_22_PHD
 import qualified Evolution_23_PostDoc
 import qualified Evolution_24_TenuredProfessor
 
+import qualified Data.MemoCombinators as Memo
+
 import Criterion.Main
 
+facFast :: Int -> Int
+facFast = Memo.integral fac'
+ where fac' 0 = 1
+       fac' n = n * fac' (n-1)
+
+
+
 main = defaultMain [ bgroup "fact" [
+       
          bench "Freshman"                $ nf Evolution_01_Freshman.fac                8
+  {-
        , bench "Sophomore"               $ nf Evolution_02_Sophomore.fac               8
        , bench "Junior"                  $ nf Evolution_03_Junior.fac                  8
        , bench "Junior2"                 $ nf Evolution_04_Junior2.fac                 8
        , bench "Senior"                  $ nf Evolution_05_Senior.fac                  8
        , bench "Senior2"                 $ nf Evolution_06_Senior2.fac                 8
        , bench "Senior3"                 $ nf Evolution_07_Senior3.fac                 8
+ -}
        , bench "Memoizing"               $ nf Evolution_08_Memoizing.fac               8
+ {-
        , bench "Pointless"               $ nf Evolution_09_Pointless.fac               8
        , bench "Iterative"               $ nf Evolution_10_Iterative.fac               8
        , bench "IterativeOneLiner"       $ nf Evolution_11_IterativeOneLiner.fac       8
@@ -52,4 +66,6 @@ main = defaultMain [ bgroup "fact" [
        , bench "PHD"                     $ nf Evolution_22_PHD.fac                     8
        , bench "PostDoc"                 $ nf Evolution_23_PostDoc.fac                 8
        , bench "TenuredProfessor"        $ nf Evolution_24_TenuredProfessor.fac        8
+-}
+       , bench "Memo combinator"         $ nf facFast        8
        ] ]
